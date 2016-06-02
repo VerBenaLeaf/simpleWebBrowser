5c87692ed50d295da048e2c5c3f89c1b13b10394
Dakyeong Lee <dlekrud0503@gmail.com>
2016-06-02 19:31:13 +0900

    #1 UI Design

--- app/src/main/java/kr/hs/emirim/good005003/simplmwebbrowser/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/good005003/simplmwebbrowser/MainActivity.java
@@ -2,12 +2,29 @@ package kr.hs.emirim.good005003.simplmwebbrowser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText editUrl;
+    Button butMove, butPrev;
+    WebView web;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editUrl = (EditText) findViewById(R.id.edit_url);
+        butPrev = (Button) findViewById(R.id.but_prev);
+        butMove = (Button) findViewById(R.id.but_move);
+        web = (WebView) findViewById(R.id.web);
+        butMove.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
